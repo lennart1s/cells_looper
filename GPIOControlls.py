c21faf6192cf4bdf20735fef1a622a803d360637
lennart1s <sandbothe.lennart@gmail.com>
2020-02-26 14:24:47 +0100

    change from gpiozero to rpi.gpio

--- GPIOControlls.py
+++ GPIOControlls.py
@@ -1,11 +1,10 @@
-from gpiozero import Button
+import RPi.GPIO as GPIO
 
 controls = {}
-
+GPIO.setMode(GPIO.BCM)
 
 class Control:
     pin = ''
-    btn = 0
     isPressed = False
 
     function = None
@@ -13,7 +12,6 @@ class Control:
 
     def __init__(self, pin, function, args):
         self.pin = pin
-        self.btn = Button(pin)
         self.function = function
         self.args = args
 
@@ -22,6 +20,7 @@ def addControll(pin, function, args):
     if callable(function):
         if pin in controls:
             print("Warning: Used '"+ pin + "'-pin for multiple controlls, only last will be used.")
+        GPIO.setup(pin, GPIO.IN)
         controls[pin] = Control(pin, function, args)
     else:
         print("GPIOEvent for '" + pin + "'-pin has to be callable.")
@@ -30,7 +29,7 @@ def addControll(pin, function, args):
 def checkForInput():
     for pin in controls:
         control = controls[pin]
-        if control.btn.is_pressed:
+        if GPIO.input(pin) == 1:
             if not control.isPressed:
                 control.isPressed = True
                 if control.args is not None:
