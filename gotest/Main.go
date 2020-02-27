25947664486278c09938a372f76c120eb9f060dc
lennart1s <sandbothe.lennart@gmail.com>
2020-02-27 19:08:35 +0100

    mc

--- gotest/Main.go
+++ gotest/Main.go
@@ -5,8 +5,10 @@ import "github.com/gordonklaus/portaudio"
 func main() {
 	portaudio.Initialize()
 	defer portaudio.Terminate()
-	data := make([]int32, 64)
-	stream, err := portaudio.OpenDefaultStream(1, 1, 44100, len(data), data)
+	in := make([]int32, 64)
+	out := make([]int32, 64)
+	stream, err := portaudio.OpenDefaultStream(1, 1, 44100, len(data), in, out)
+	stream, err := portautio.OpenStream()
 	if err != nil {
 		panic(err)
 	}
@@ -14,6 +16,7 @@ func main() {
 	stream.Start()
 	for {
 		stream.Read()
+		out = in
 		stream.Write()
 	}
 }
