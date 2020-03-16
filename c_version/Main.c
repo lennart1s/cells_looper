3f878f5c288089dc25f8227a7076ad4bf40bddca
lennart1s <sandbothe.lennart@gmail.com>
2020-03-16 12:51:03 +0100

    first test

--- c_version/Main.c
+++ c_version/Main.c
@@ -3,6 +3,14 @@
 
 int main() {
     printf("Hello World\n");
+    Pa_Initialize();
     return 0;
+}
+
+static int callback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, 
+                        const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags,
+                        void *userData) {
+
+    printf(framesPerBuffer);
 }
 
